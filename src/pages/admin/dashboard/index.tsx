4e75d021a5d28b75716ab2bcb855f2c0ab8e88b6
X-DSI <xmandsi@gmail.com>
2022-11-21 18:47:42 +0530

    ⏫ UPDATE: admin song page

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { trpc } from "../../../utils/trpc";
 
 const DashboardPage = () => {
   return <div>DashboardPage</div>;
--- src/pages/admin/song/index.tsx
+++ src/pages/admin/song/index.tsx
@@ -1,8 +1,25 @@
 import React, { type ReactElement } from "react";
 import AdminLayout from "../../../layouts/AdminLayout";
+import { trpc } from "../../../utils/trpc";
 
 const SongPage = () => {
-  return <div>Song Page</div>;
+  const { data } = trpc.song.getAll.useQuery();
+  return (
+    <div>
+      <div>Song Page</div>
+      <div className="admin-song-wrap">
+        {data && (
+          <div>
+            {data.map((song) => (
+              <div key={song.id}>
+                <p>{song.id}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 SongPage.getLayout = (page: ReactElement) => {
