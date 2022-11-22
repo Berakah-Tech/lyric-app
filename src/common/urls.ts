a652fe72cf9fed583a2789a795f142790fbb841f
Jerome Marshall <jeromemarshall0@gmail.com>
2022-11-22 19:56:35 +0530

    ⏫ UPDATE: urls

--- src/common/urls.ts
+++ src/common/urls.ts
@@ -1,4 +1,6 @@
-export const adminBookPage = "/admin/book";
-export const adminSongPage = "/admin/song";
-export const adminCategoryPage = "/admin/category";
-export const adminUsersPage = "/admin/user";
+export const adminPage = "/admin";
+export const adminBookPage = adminPage + "/book";
+export const adminSongPage = adminPage + "/song";
+export const adminSongCreatePage = adminSongPage + "/create";
+export const adminCategoryPage = adminPage + "/category";
+export const adminUsersPage = adminPage + "/user";
