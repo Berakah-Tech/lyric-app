31adb882fd198f2f83913047acd79c25ecba1fc0
Jerome Marshall <jeromemarshall0@gmail.com>
2022-11-28 12:31:10 +0530

    ⏫ UPDATE: logger link config

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -18,8 +18,8 @@ export const trpc = createTRPCNext<AppRouter>({
       links: [
         loggerLink({
           enabled: (opts) =>
-            process.env.NODE_ENV === "development" ||
-            (opts.direction === "down" && opts.result instanceof Error),
+            // process.env.NODE_ENV === "development" ||
+            opts.direction === "down" && opts.result instanceof Error,
         }),
         httpBatchLink({
           url: `${getBaseUrl()}/api/trpc`,
