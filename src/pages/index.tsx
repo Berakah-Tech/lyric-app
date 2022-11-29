3b3c967b9ed7a091282c5c4b764ceff4dd65a3c9
X-DSI <xmandsi@gmail.com>
2022-11-29 12:35:10 +0530

    ✨ NEW: Added Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import { type ReactElement } from "react";
 import Layout from "../components/layouts/Layout";
@@ -6,6 +7,9 @@ import type { NextPageWithLayout } from "../types/types";
 const Home: NextPageWithLayout = () => {
   return (
     <>
+      <Head>
+        <title>Lyric App</title>
+      </Head>
       <div className="grid">
         <p>Home Page</p>
         <Link href={"/admin"} className="bg-black p-2 text-white">
